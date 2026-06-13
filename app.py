c4629c358f8d46c7217782971c7b80f076c28da0
Md Shahil <mdsahilshaikh1506@gmail.com>
2026-06-13 10:20:09 +0530

    Save Changes

--- app.py
+++ app.py
@@ -75,5 +75,5 @@ def update_task():
     return redirect(url_for('index'))
 
 if __name__ == "__main__":
-    threading.Thread(target=run_scheduler, daemon=True).start()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
